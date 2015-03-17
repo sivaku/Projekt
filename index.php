705d3d0171a4ccdd6222c2bdc886405c1e508024
unknown <s.kiru@hotmail.com>
2015-03-17 12:49:34 +0100

    Neues Projekt WebProgramming

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <title>CellPhone Shit</title>
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="main">
+  <div class="header">
+  <h1 align="left">CellPhone Shit</h1>
+  <p align="right">
+    <img src="img/warenkorb-icon.jpg" width="35" height="25">Warenkorb
+	<img src="img/icon_register.jpg" width="35" height="25">Anmelden
+  </p>
+  
+	<div class="navigation">
+	  <div align="right">
+	    <?php include("pages/navi.html");?>
+	  </div>
+    </div>
+   </div>	
+	<div class="clear">ZeilenSprung</div>
+	<div class="slider">
+	  <div align="center">
+	    <img src="img/iphone2.jpg" width="1000" height="400">
+	  </div>
+    </div>
+
+  <div class="clear">ZeilenSprung</div>
+     
+  
+ 
+      <div class="sidebarleft">
+		<p><a href="#">Beispiel 1</a></p>
+		<p><a href="#">Beispiel 2</a></p>
+		<p><a href="#">Beispiel 3</a></p>
+      </div>
+	
+	  <div class="contentBox">
+	    <h2 align="left">Handys</h2>
+	    <p>
+		<table cellpadding="25px" align="center">
+		<tr>
+		<td>
+		  <div class="products">
+		    <p align="center">
+			  <img src="img/iphone_6_space_gray.png" width="130px" height="100px">
+			  <h2>Iphone 6</h2>
+			  <h3><b>CHF 600</b></h3>
+			  <p>Fabrik neu</p>
+			</p>
+		  </div>
+		</td>
+        <td>		
+		  <div class="products">
+		    <p align="center">
+			  <img src="img/galaxy_s6_edge_black.png" width="130px" height="100px">
+			  <h2>Samsung S6</h2>
+			  <h3><b>CHF 580</b></h3>
+			  <p>Fabrik neu</p>
+	        </p>
+		  </div>
+		</td>
+		<td>
+		  <div class="products">
+		    <p align="center">
+			  <img src="img/htc_one_m9_silver.png" width="130px" height="100px">
+			  <h2>HTC One M9 silver on gold</h2>
+			  <h3><b>CHF 520</b></h3>
+			  <p>Nicht mehr Original verpackt<p>
+			</p>
+		  </div>
+		</td>
+		<td>
+		  <div class="products">
+		    <p align="center">
+			  <img src="img/lumia_930_black.png" width="130px" height="100px">
+			  <h2>Nokia Lumia 930 schwarz</h2>
+			  <h3><b>CHF 520</b></h3>
+			  <p>Nicht mehr Original verpackt<p>
+			</p>
+		  </div>
+		</td>
+		</tr>
+        <tr>
+		<td>
+		  <div class="products">
+		    <p align="center">
+			  <img src="img/connect_basic.png" width="130px" height="100px">
+			  <h2>Emporia Connect Basic</h2>
+			  <h3><b>CHF 150</b></h3>
+			  <p>Gebraucht<p>
+			</p>
+		  </div>
+		</td>
+		<td>
+		  <div class="products">
+		    <p align="center">
+			  <img src="img/lg_g3_black.png" width="130px" height="100px">
+			  <h2>LG G3 16GB schwarz</h2>
+			  <h3><b>CHF 520</b></h3>
+			  <p>Original verpackt<p>
+			</p>
+		  </div>
+		</td>
+        </tr>		
+	    </table>
+	      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
+          Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
+          Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.         
+		</p>
+      </div>
+	
+	  <div class="sidebarright">
+	    <p><a href="#">Beispiel 4</a></p>
+	    <p><a href="#">Beispiel 5</a></p>
+	    <p><a href="#">Beispiel 6</a></p>
+	  </div>
+  
+
+  <div class="footer">
+    <p align="center">Copyright &copy;  2015 by CellPhone Shit</p>
+  </div>
+
+</div>
+</body>
+</html>
---
+++ pages/kontakt.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+
+</head>
+
+<body>
+<?php_egg_logo_guid
+
+/* Betreff und E-Mail Variable */
+
+	$emailBetreff = 'CellPhone Shit';
+	$webmaster = 'info@cellphone.ch';
+	
+/* Emailform Daten */
+ 
+Name: 
+Email:
+Betreff:
+Nachrichten:
+
+
+/* Antwort */
+
+echo "Vielen Dank für Ihre Nachricht an CellPhone Shit. Ihr Nachricht wurde erfolgreich versendet!";
+
+
+?>
+</body>
+</html>
